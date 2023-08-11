cfc2eeb51f7c9cecc7dae7363b6f21e6c8b3a5cb
hajarZiya <hajarziya2839@gmail.com>
2023-08-11 15:18:36 +0300

    added 3 questions

---
+++ week-7/Q01_cw2/app.js
@@ -0,0 +1,11 @@
+const sum = (x, y, ...z) => {
+  const args = [x, y, ...z];
+
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+};
+
+console.log(sum(1, 2, 3, 4, 5));
