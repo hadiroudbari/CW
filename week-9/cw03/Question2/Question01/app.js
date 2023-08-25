7cff134519dc8bfaf1d8a96b43f50c71d77c37a6
HajarZiya <116388510+hajarziya@users.noreply.github.com>
2023-08-25 15:05:33 +0300

    added q01 cw3

--- week-9/cw03/Question2/Question01/app.js
+++
@@ -1,8 +0,0 @@
-function checklsDate(date) {
-  return date instanceof Date;
-}
-
-console.log(checklsDate("October 13, 2014 11 : 13:00"));
-console.log(checklsDate(new Date(86400000)));
-console.log(checklsDate(new Date(99, 5, 24, 11)));
-console.log(checklsDate([1, 2, 4, 0]));
